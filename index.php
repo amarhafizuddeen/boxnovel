6ce668b585bb526cf5276f98ab97f4737415ab49
Amar <amarhafizuddeen@gmail.com>
2019-03-23 23:58:11 +0800

    Added white robed chief.

--- index.php
+++ index.php
@@ -93,7 +93,7 @@ function chapterNavigation($novel, $chapter, $isLatestChapter = false){
 
 function getHomePage(){
 	global $curl;
-	$novels = array("king-of-gods", "the-legend-of-futian", "reincarnation-of-the-strongest-sword-god", "library-of-heavens-path", "mmorpg-martial-gamer");
+	$novels = array("white-robed-chief", "king-of-gods", "the-legend-of-futian", "reincarnation-of-the-strongest-sword-god", "library-of-heavens-path", "mmorpg-martial-gamer");
 	$str = "";
 	
 	foreach ($novels as $novel) { 
